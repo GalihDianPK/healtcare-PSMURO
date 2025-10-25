b67c013770330f287831733c438e9158dc5905ef
Galih Dian Permana Kusuma <galihdpk0204@gmail.com>
2025-10-25 16:57:36 +0700

    Update Percobaan 18B20 + LCD.c

--- Percobaan 18B20 + LCD.c
+++ Percobaan 18B20 + LCD.c
@@ -19,7 +19,7 @@ Serial.println
 // Start up the library
 sensors.begin();
 // Initialize the LCD
-lcd.begin();
+lcd.init();
 // Turn on the backlight
 lcd.backlight();
 lcd.print(" WELCOME ");
@@ -30,6 +30,7 @@ lcd.clear();
 lcd.setCursor(0, 0);
 lcd.print("PSMURO");
 lcd.setCursor(0, 1);
+
 lcd.print("UNIV.GUNADARMA");
 delay(5000);
 lcd.clear();
@@ -61,85 +62,4 @@ lcd.setCursor(0, 1); // Set cursor to second line
 lcd.print(tempC);
 lcd.print(" C");
 delay(1000);
-}
-lcd.print("PSMURO");
-lcd.setCursor(0, 1);
-lcd.print("UNIV.GUNADARMA");
-delay(5000);
-lcd.clear();
-lcd.setCursor(0, 0);
-lcd.print("MCU HEALTHCARE");
-lcd.setCursor(0, 1);
-lcd.print("HEART RATE");
-delay(5000);
-lcd.clear();
-// Attempt to initialize the MAX30105 sensor. Check for asuccessful connection and report.
-if (!particleSensor.begin(Wire, I2C_SPEED_FAST))
-{
-// Start communication using fast I2C speed
-Serial.println("MAX30102 was not found. Please check
-wiring/power. ");
-while (1); // Infinite loop to halt further execution ifsensor is not found
-}
-Serial.println("Place your index finger on the sensor with
-steady pressure.");
-particleSensor.setup();
-// Configure sensor with default settings for heart ratemonitoring
-particleSensor.setPulseAmplitudeRed(0x0A);
-// Set the red LED pulse amplitude (intensity) to a low valueas an indicator
-particleSensor.setPulseAmplitudeGreen(0);
-// Turn off the green LED as it's not used here
-}
-void loop()
-{
-long irValue = particleSensor.getIR();
-// Read the infrared value from the sensor
-if (checkForBeat(irValue) == true)
-{
-// Check if a heart beat is detected
-long delta = millis() - lastBeat;
-// Calculate the time between the current and last beat
-lastBeat = millis(); // Update lastBeat to the current time
-beatsPerMinute = 60 / (delta / 1000.0);
-// Calculate BPM
-// Ensure BPM is within a reasonable range before updatingthe rates array
-if (beatsPerMinute < 255 && beatsPerMinute > 20)
-{
-rates[rateSpot++] = (byte)beatsPerMinute;
-// Store this reading in the rates array
-rateSpot %= RATE_SIZE;
-// Wrap the rateSpot index to keep it within the boundsof the rates array
-// Compute the average of stored heart rates to smooth outthe BPM
-beatAvg = 0;
-for (byte x = 0 ; x < RATE_SIZE ; x++)
-beatAvg += rates[x];
-beatAvg /= RATE_SIZE;
-}
-}
-Serial.print("IR=");
-Serial.print(irValue);
-Serial.print(", BPM=");
-Serial.print(beatsPerMinute);
-Serial.print(", Avg BPM=");
-Serial.print(beatAvg);
-if (irValue < 50000)
-{
-Serial.print(" No finger?");
-lcd.setCursor(0, 0);
-lcd.print("No Finger ? ");
-lcd.setCursor(0, 1);
-lcd.print("Letakan Jari! ");
-}
-else
-{
-lcd.clear();
-lcd.setCursor(0, 0);
-lcd.print("BPM: ");
-lcd.print(beatsPerMinute);
-lcd.setCursor(0, 1);
-lcd.print("Avg BPM: ");
-lcd.print(beatAvg);
-}
-Serial.println();
-//delay(500); // Delay to update the displayand avoid flickering
 }
